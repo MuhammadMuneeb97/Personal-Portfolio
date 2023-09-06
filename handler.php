d9fe369dfcb538cf328e8751af7f92e731d438a5
Hafiz Muhammad Muneeb <mmuneeb765@gmail.com>
2023-09-06 11:38:46 +0500

    5th change

--- handler.php
+++ handler.php
@@ -10,45 +10,45 @@ require 'php/PHPMailer-5.2.28/src/SMTP.php';
 $mail = new PHPMailer(true);
 $mail_to_email = 'mmuneeb765@gmail.com'; // your email
 $mail_to_name = 'Webmaster';
+$mail_title = 'Portfolio Contact'; // Email subject
 
 try {
 
-	$mail_from_name = isset( $_POST['name'] ) ? $_POST['name'] : '';
-	$mail_from_email = isset( $_POST['email'] ) ? $_POST['email'] : '';
-	$mail_message = isset( $_POST['message'] ) ? $_POST['message'] : '';
+    $mail_from_name = isset($_POST['name']) ? $_POST['name'] : '';
+    $mail_from_email = isset($_POST['email']) ? $_POST['email'] : '';
+    $mail_message = isset($_POST['message']) ? $_POST['message'] : '';
 
+    // Server settings
+    $mail->isSMTP(); // Send using SMTP
+    $mail->Host = 'smtp.***.com'; // Set the SMTP server to send through
+    $mail->SMTPAuth = true; // Enable SMTP authentication
+    $mail->Username = '***'; // SMTP username
+    $mail->Password = '***'; // SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port = 465; // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
-	// Server settings
-	$mail->isSMTP(); // Send using SMTP
-	$mail->Host = 'smtp.***.com'; // Set the SMTP server to send through
-	$mail->SMTPAuth = true; // Enable SMTP authentication
-	$mail->Username = '***'; // SMTP username
-	$mail->Password = '***'; // SMTP password
-	$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-	$mail->Port = 465; // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+    $mail->setFrom($mail_to_email, $mail_to_name); // Your email
+    $mail->addAddress($mail_from_email, $mail_from_name); // Add a recipient
 
-	$mail->setFrom($mail_to_email, $mail_to_name); // Your email
-	$mail->addAddress($mail_from_email, $mail_from_name); // Add a recipient
+    // for($ct=0; $ct<count($_FILES['file_attach']['tmp_name']); $ct++) {
+    // 	$mail->AddAttachment($_FILES['file_attach']['tmp_name'][$ct], $_FILES['file_attach']['name'][$ct]);
+    // }
 
-	// for($ct=0; $ct<count($_FILES['file_attach']['tmp_name']); $ct++) {
-	// 	$mail->AddAttachment($_FILES['file_attach']['tmp_name'][$ct], $_FILES['file_attach']['name'][$ct]);
-	// }
+    // Content
+    $mail->isHTML(true); // Set email format to HTML
 
-	// Content
-	$mail->isHTML(true); // Set email format to HTML
+    $mail->Subject = $mail_title;
+    $mail->Body = '
+        <strong>Name:</strong> ' . $mail_from_name . '<br>
+        <strong>Email:</strong> ' . $mail_from_email . '<br>
+        <strong>Message:</strong> ' . $mail_message;
 
-	$mail->Subject = $mail_title;
-	$mail->Body = '
-		<strong>Name:</strong> ' . $mail_from_name . '<br>
-		<strong>Email:</strong> ' . $mail_from_email . '<br>
-		<strong>Message:</strong> ' . $mail_message;
+    $mail->send();
 
-	$mail->Send();
-
-	echo 'Message has been sent';
+    echo 'Message has been sent';
 
 } catch (Exception $e) {
 
-	echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 
 }
